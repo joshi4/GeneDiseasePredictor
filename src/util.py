600f9e60bf4facc27006d54e04e9eb0b043ef3ae
Shantanu <joshishantanu4@gmail.com>
2014-11-14 21:14:16 -0800

    added in SVM and logistic regression, logistic regression is buggygit add logistic_regression.py

---
+++ src/util.py
@@ -0,0 +1,42 @@
+import collections
+import pickle 
+
+
+# taken dot_product and increment from the sentiment assignment 
+def dot_product(d1,d2):
+    if len(d1) < len(d2):
+        return dot_product(d2,d1)
+    return sum(d1.get(f, 0) * v for f, v in d2.items())
+
+def increment(d1,scale,d2):
+    """
+    Implements d1 += scale * d2 for sparse vectors.
+    @param dict d1: the feature vector which is mutated.
+    @param float scale
+    @param dict d2: a feature vector.
+    """
+    for f, v in d2.items():
+        d1[f] = d1.get(f, 0) + v * scale
+
+def evaluate(pickled_test_file,predictor):
+    test_examples = pickle.load(open(pickled_test_file, 'rb'))
+    errors = 0 
+    false_positive = 0 
+    false_negative = 0 
+    N = len(test_examples) 
+    for feature,label in test_examples:
+        result = predictor(feature)
+        if result != label:
+            errors += 1
+        if result == 1 and label == -1 :
+            false_positive += 1 
+        if result == -1  and label == 1:
+            false_negative += 1
+
+    error_percent = errors*100.0/N 
+    false_negative_percent = false_negative *100.0/N
+    false_positive_percent = false_positive *100.0/N
+    print "You misclassified %d out of %d for an error percent of %f" %(errors, N, error_percent) 
+    print "false positves = %d or in percent: %f"%(false_positive, false_positive_percent) 
+    print "false negatves = %d or in percent: %f"%(false_negative, false_negative_percent) 
+
