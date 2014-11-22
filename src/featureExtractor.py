13ba70963aeda20b163acccf713e323babb45963
Justin Womersley <justinwomersley@gmail.com>
2014-11-21 16:05:22 -0800

    Finished faster overlap feature Extractor

--- src/featureExtractor.py
+++ src/featureExtractor.py
@@ -24,8 +24,7 @@ files = [(input_diseased_bed_file, 1.0), (input_healthy_bed_file, -1.0)]
 # A list of feature extracting functions (one for each feature). 
 # Each must take the .bed line as argument, and return the key that is to be set to 1
 # These functions are defined in features.py and must have the same argument and return type structure
-listOfFeatures = [features.chromosome, features.cnvLength, features.svType, \
-        features.overlapWithCodingExonsFast, features.startPosition, features.endPosition]
+listOfFeatures = [features.chromosome, features.cnvLength, features.svType, features.startPosition, features.endPosition]
 BaseLineFeatures = [features.overlapWithCodingExonsFastest, features.chromosome, features.cnvLength, features.svType]
 
 dataTesting = []
