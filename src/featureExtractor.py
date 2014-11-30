dcd18732281f1731b4c6a10819518ec2fa8af51b
Justin Womersley <justinwomersley@gmail.com>
2014-11-30 12:38:25 -0800

    Removing known repeats, file too large. Add comments

--- src/featureExtractor.py
+++ src/featureExtractor.py
@@ -12,25 +12,26 @@ trainingSet.p
 testingSet.p
 """
 
+# The dataset is split into training and testing sets
 PercentageOfSetForTraining = 80 # A percentage
 
 # Two input files, diseased or healthy in .bed format
 input_diseased_bed_file = "../dbVarData/nstd100.diseased.vcf.bed"
 input_healthy_bed_file = "../dbVarData/nstd100.healthy.vcf.bed"
 
-# To loop through the files
+# To loop through the two files
 files = [(input_diseased_bed_file, 1.0), (input_healthy_bed_file, -1.0)]
 
 # A list of feature extracting functions (one for each feature). 
 # Each must take the .bed line as argument, and return the key that is to be set to 1
 # These functions are defined in features.py and must have the same argument and return type structure
-listOfFeatures = [features.chromosome, features.cnvLength, features.svType, features.startPosition, features.endPosition]
-BaseLineFeatures = [features.absoluteStartPosition, features.overlapWithKnownRepeats, features.overlapWithMicroSats, features.overlapWithKnownGenes, features.overlapWithVistaEnhancer, features.overlapWithCodingExons, features.chromosome, features.cnvLength, features.svType]
+listOfFeatures = [features.absoluteStartPosition, features.overlapWithKnownRepeats, features.overlapWithMicroSats, features.overlapWithKnownGenes, features.overlapWithVistaEnhancer, features.overlapWithCodingExons, features.chromosome, features.cnvLength, features.svType]
 
+# To save the results (sparse representation of features)
 dataTesting = []
 dataTraining = []
 
-# Loop through each file
+# Loop through each file (healthy & diseased)
 for (file, result) in files:
 	# Each line is a tuple of (features, result) where:
 	#	features is a collection of features (sparse representation)
@@ -44,22 +45,19 @@ for (file, result) in files:
 		sparseFeatures = collections.Counter()
 		lineList = line.split()
 		
-		for featureFunc in BaseLineFeatures: #BaselineFeatures
+		# For each feature extraction function, run it on this input line
+		for featureFunc in listOfFeatures:
 			feature = featureFunc(lineList)
 			if feature:
 				(key, value) = feature
 				if key: sparseFeatures[key] = value
 
-		# Add this entry into the training/testing set
+		# Add this entry into the training or testing set
 		if(random.randint(1,100) <= PercentageOfSetForTraining):
 			dataTraining.append((sparseFeatures, result))
 		else:
 			dataTesting.append((sparseFeatures, result))
-		# print (sparseFeatures, result)
 
-#print "Training data: %s " % dataTraining
-#print "Testing data: %s " % dataTesting
-# Save the results to pickle files
+# Save the results to pickle files for later use
 pickle.dump(dataTraining, open("trainingSet.p", "wb"))	
 pickle.dump(dataTesting, open("testingSet.p", "wb"))
-
--- src/features.py
+++ src/features.py
@@ -1,32 +1,37 @@
 import os
 import pickle
+import math
 
 """
 This file has a function for each feature that we want to extract
 
 They must follow the structure:
 
-argument: 1 argument being the information in the bed line, split by whitespace (a list)
-return: a (key, value) tuple where key is the key and value is the features value (this is 1 for indicator variables)
+argument:
+	1 argument being the information in the bed line in a list
+return:
+	a (key, value) tuple where key is the feature's key and value is the features value (the value is simply 1 for indicator variables)
 """
 
 overlapSelectPath = "../../../tools/overlapSelect"
 
 def startPosition(bedLine):
 	"""
+	Start Position of the CNV in its chromosome
 	"""
 	start = int(bedLine[1])
 	return ("start", start)
 
 def endPosition(bedLine):
 	"""
+	End position of the CNV in its chromosome
 	"""
 	end = int(bedLine[2])
 	return ("end", end)
 
 def chromosome(bedLine):
 	"""
-	Chromosome number feature. (indicator variable)
+	Chromosome number. (indicator variable)
 	Example keys:
 	"chrom1" or "chrom2" or "chrom20"
 	"""
@@ -35,7 +40,7 @@ def chromosome(bedLine):
 
 def absoluteStartPosition(bedLine):
 	"""
-	Creates a feature for the start position of the CNV in the entire genome.
+	Aboslute start position of the CNV (in the entire genome.)
 	"""
 	start = int(bedLine[1])
 	chrom = bedLine[0].lower()
@@ -74,16 +79,27 @@ def absoluteStartPosition(bedLine):
 
 def length(bedLine):
 	"""
+	The length in basepairs of teh CNV
 	"""
 	start = int(bedLine[1])
 	end = int(bedLine[2])
 	length = end - start
 	return ("length", length)
 
+def logLength(bedLine):
+	"""
+	The log (base 10) of the CNV
+	"""
+	start = int(bedLine[1])
+	end = int(bedLine[2])
+	length = end - start
+	logLength = math.log(10, length)
+	return ("logLength", logLength)	
+
 # TODO, need to calculate better thresholds for length, use histogram to equally seperate them?
 def cnvLength(bedLine):
 	"""
-	Length of CNV, grouped into 5 equally sized groups* Still need to work out group thresholds
+	Length of CNV, grouped into 5 equally sized groups (indicator variables)
 	"""
 	start = int(bedLine[1])
 	end = int(bedLine[2])
@@ -104,7 +120,7 @@ def cnvLength(bedLine):
 
 def svType(bedLine):
 	"""
-	The type of CNV change - insertion or duplicate or deletion
+	The type of CNV change - insertion or duplicate or deletion (indicator variable)
 	"""
 	info = bedLine[3].split(";")
 	svType = info[0].lower()
@@ -114,7 +130,7 @@ diseasedOverlapWithExons = pickle.load(open('../overlapBEDFiles/knownGenesCoding
 healthyOverlapWithExons = pickle.load(open('../overlapBEDFiles/knownGenesCodingExons/healthy.p' ,'rb'))
 def overlapWithCodingExons(bedLine):
 	"""
-	
+	How many times the CNV overlaps with a Known Coding Exon
 	"""
 	info = bedLine[3].split(";")
 	uniqeId = info[1]
@@ -129,7 +145,7 @@ diseasedOverlapWithRegulatoryVistaEnhancers = pickle.load(open('../overlapBEDFil
 healthyOverlapWithRegulatoryVistaEnhancers = pickle.load(open('../overlapBEDFiles/RegulatoryVistaEnhancers/healthy.p' ,'rb'))
 def overlapWithVistaEnhancer(bedLine):
 	"""
-	
+	How many times the CNV overlaps with a Vista Enhancer
 	"""
 	info = bedLine[3].split(";")
 	uniqeId = info[1]
@@ -144,7 +160,7 @@ diseasedOverlapWithRegulatoryBroadEnhancers = pickle.load(open('../overlapBEDFil
 healthyOverlapWithRegulatoryBroadEnhancers = pickle.load(open('../overlapBEDFiles/RegulatoryBroadEnhancers/healthy.p' ,'rb'))
 def overlapWithBroadEnhancer(bedLine):
 	"""
-	
+	How many times the CNV overlaps with the Broad Enhancer set
 	"""
 	info = bedLine[3].split(";")
 	uniqeId = info[1]
@@ -159,7 +175,7 @@ diseasedOverlapWithKnownGenes = pickle.load(open('../overlapBEDFiles/KnownGenes/
 healthyOverlapWithKnownGenes = pickle.load(open('../overlapBEDFiles/KnownGenes/healthy.p' ,'rb'))
 def overlapWithKnownGenes(bedLine):
 	"""
-	
+	How many times the CNV overlaps with a Known Gene
 	"""
 	info = bedLine[3].split(";")
 	uniqeId = info[1]
@@ -174,7 +190,7 @@ diseasedOverlapWithMicroSeq = pickle.load(open('../overlapBEDFiles/Microsatellit
 healthyOverlapWithMicroSeq = pickle.load(open('../overlapBEDFiles/Microsatellites/healthy.p' ,'rb'))
 def overlapWithMicroSats(bedLine):
 	"""
-	
+	How many times the CNV overlaps with a microsatellite sequence
 	"""
 	info = bedLine[3].split(";")
 	uniqeId = info[1]
@@ -189,7 +205,7 @@ diseasedOverlapWithKnownRepeats = pickle.load(open('../overlapBEDFiles/KnownRepe
 healthyOverlapWithKnownRepeats = pickle.load(open('../overlapBEDFiles/KnownRepeats/healthy.p' ,'rb'))
 def overlapWithKnownRepeats(bedLine):
 	"""
-
+	How many times the CNV overlaps with a known repeat Sequence
 	"""
 	info = bedLine[3].split(";")
 	uniqeId = info[1]
